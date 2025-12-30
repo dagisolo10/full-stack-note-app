76a34375ad0046627711d9423263d5cfdadec779
dagisolo10 <dagisolo1023l@gmail.com>
2025-12-30 13:22:01 +0300

    responsive fix

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -11,12 +11,12 @@ const Notes = () => {
         <div className="w-full px-4 sm:px-6 lg:px-8 mb-12 sm:mb-16 pt-4 sm:pt-6">
             <div className="note-grid mx-auto">
                 {notes.length === 0 ? (
-                    <div className="col-span-full flex flex-col items-center justify-center py-20 ">
+                    <div className="col-span-full flex flex-col gap-4 items-center justify-center py-20 ">
                         <p className="text-xl sm:text-2xl mb-6 text-gray-400 opacity-50">No notes yet. Start creating!</p>
                         <Link to="/add-note">
-                            <p className="text-primary underline text-2xl text-center">Create your first note</p>
+                            <p className="text-primary-content text-2xl text-center">Create your first note</p>
                         </Link>
-                        <Notebook className="size-16 text-center text-primary" />
+                        <Notebook className="size-16 p-6 bg-primary text-center text-primary-content" />
                     </div>
                 ) : (
                     notes.map((note) => <NoteCard setNotes={setNotes} key={note._id} note={note} />)
