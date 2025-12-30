d43dfffa6b132be4c75fe4b3601916ca01997cdf
dagisolo10 <dagisolo1023l@gmail.com>
2025-12-30 13:16:16 +0300

    responsive fix

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import NoteCard from "../components/NoteCard";
 import { Link } from "react-router";
+import { Notebook } from "lucide-react";
 
 const Notes = () => {
     const { notes, setNotes } = useContext(AuthContext);
@@ -12,8 +13,9 @@ const Notes = () => {
                 {notes.length === 0 ? (
                     <div className="col-span-full flex flex-col items-center justify-center py-20 opacity-50">
                         <p className="text-xl sm:text-2xl text-gray-400">No notes yet. Start creating!</p>
+                        <Notebook className="size-8 text-center text-primary" />
                         <Link>
-                            <p className="btn btn-link text-center">Create your first note</p>
+                            <p className="text-primary underline text-2xl text-center">Create your first note</p>
                         </Link>
                     </div>
                 ) : (
